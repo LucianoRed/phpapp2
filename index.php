1c417fd19a8febaa6fb7f202982d525893cc1afd
LucianoRed <lscorsin@redhat.com>
2023-09-20 12:27:49 -0300

    Fix imagens

--- index.php
+++ index.php
@@ -1044,7 +1044,7 @@
                 fetchAndUpdateSpan();
                 fetchAndUpdateSpan2();
 
-            }, 5000); // 5000 milliseconds = 5 seconds
+            }, 10000); // 5000 milliseconds = 5 seconds
 
             function fetchAndUpdateSpan() {
             const url = 'https://roadshow-2023-git2-upload.apps.teste.sandbox2448.opentlc.com/contagem.php'; // Replace with the URL you want to fetch data from
